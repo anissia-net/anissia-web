7f41de03f8540c6896d4e5294fb4140a9c0c5304
CodeName393 <62503817+CodeName393@users.noreply.github.com>
2024-08-13 11:33:40 +0900

    Update 2024.vue
    반응형 개선, 디자인 개선, 애니메이션 추가, 코드 버그 수정, 미사용 태그 삭제

--- src/page/schedule/2024.vue
+++ src/page/schedule/2024.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="bg-white dark:bg-black text-zinc-800 dark:text-zinc-400">
-    <div class=" container pb-10 m-auto px-1 sm:px-2 md:px-4 duration-300 min-h-screen py-0" @click="e => evtClickClosePopup(e)">
+  <div class="duration-300 bg-white dark:bg-black">
+    <div class="container duration-300 pb-10 m-auto px-1 sm:px-2 md:px-4 min-h-screen py-0 text-zinc-800 dark:text-zinc-400" @click="e => evtClickClosePopup(e)">
 
       <!-- 타이틀 -->
-      <div class="title font-extralight h-16 text-center flex items-center">
-        <div class="title-main text-2xl leading-none flex-1 text-center pl-9"><a href="/" target="_blank">애니<span class="mob-hide">메이션</span> 편성표</a></div>
+      <div class="font-extralight h-16 text-center flex items-center">
+        <div class="text-2xl leading-none flex-1 text-center pl-9"><a href="/" target="_blank"><span class="max-[250px]:hidden">애니</span><span class="max-[350px]:hidden">메이션</span> 편성표</a></div>
         <button class="text-lg hover:text-sky-700 dark:hover:text-gray-200 px-4 py-2.5" @click="toggleTheme()">
         <span class="dark:hidden">
           <i class="fa-solid fa-sun"></i>
@@ -21,71 +21,76 @@
       </div>
 
       <!-- 애니메이션 리스트 -->
-      <div class="mt-4">
-        <div class="mt-3 grid gap-4 md:grid-cols-2 xl:grid-cols-3">
-          <div v-for="(node, i) in animeList" class="p-4 as-box">
-            <div @click="getCaptionList(node);" class="cursor-pointer">
-              <!-- 시간 -->
-              <div v-if="node.scheduleTime != '-'" class="text-md font-bold text-blue-600 dark:text-blue-500">
-                {{node.scheduleTime}}
+      <transition name="fade">
+        <div class="mt-4" v-if="animeList.length">
+          <div class="mt-3 grid gap-4 md:grid-cols-2 xl:grid-cols-3">
+            <div v-for="(node, i) in animeList" class="p-4 as-box">
+              <div @click="getCaptionList(node);" class="cursor-pointer">
+                <!-- 시간 -->
+                <div v-if="node.scheduleTime != '-'" class="text-md font-bold text-blue-600 dark:text-blue-500">
+                  {{node.scheduleTime}}
+                </div>
+                <div class="text-md mt-1 font-bold text-gray-800 dark:text-zinc-300">
+                  <!-- 방영태그 -->
+                  <span v-if="node.subjectPrefix">[<b class="text-blue-600 dark:text-blue-500">{{node.subjectPrefix}}</b>] </span>
+                  <!-- 애니제목 -->
+                  {{node.subject}}
+                </div>
+                <!-- 원제 -->
+                <div class="text-xs mt-1" v-if="node.originalSubject">{{node.originalSubject}}</div>
               </div>
-              <div class="text-md mt-1 font-bold text-gray-800 dark:text-zinc-300">
-                <!-- 방영태그 -->
-                <span v-if="node.subjectPrefix">[<b class="text-blue-600 dark:text-blue-500">{{node.subjectPrefix}}</b>] </span>
-                <!-- 애니제목 -->
-                {{node.subject}}
+              <!-- 태그박스 -->
+              <div class="mt-1 space-x-1 space-y-2 text-gray-800 dark:text-zinc-300">
+                <span class="as-tag-xs" v-for="tag in node.tags" :key="tag">{{tag}}</span>
+                <span class="as-tag-xs" v-for="tag in node.genres.split(/,/g)" :key="tag"><router-link :to="`/anime?q=%23${encodeURIComponent(tag)}`">{{tag}}</router-link></span>
+                <span class="as-tag-xs" v-if="node.website"><a :href="node.website" target="_blank" class="fas fa-home"></a></span>
+                <span class="as-tag-xs" v-if="node.twitter"><a :href="node.twitter" target="_blank" class="fa-brands fa-x-twitter"></a></span>
+                <span class="as-tag-xs" v-if="node.captionCount"><span class="fas fa-closed-captioning">&nbsp; {{node.captionCount}}</span></span>
               </div>
-              <!-- 원제 -->
-              <div class="text-xs mt-1" v-if="node.originalSubject">{{node.originalSubject}}</div>
-            </div>
-            <div class="mt-1 space-x-1 space-y-2 text-gray-800 dark:text-zinc-300">
-              <span class="as-tag-xs" v-for="tag in node.tags" :key="tag">{{tag}}</span>
-              <span class="as-tag-xs" v-for="tag in node.genres.split(/,/g)" :key="tag"><router-link :to="`/anime?q=%23${encodeURIComponent(tag)}`">{{tag}}</router-link></span>
-              <span class="as-tag-xs" v-if="node.website"><a :href="node.website" target="_blank" class="fas fa-home"></a></span>
-              <span class="as-tag-xs" v-if="node.twitter"><a :href="node.twitter" target="_blank" class="fa-brands fa-twitter"></a></span>
-              <span class="as-tag-xs" v-if="node.captionCount"><span class="fas fa-closed-captioning">&nbsp; {{node.captionCount}}</span></span>
             </div>
           </div>
         </div>
-      </div>
+      </transition>
 
       <!-- 팝업 -->
-      <div class="fixed inset-0 backdrop-blur-sm bg-gray-800/10" v-if="animeNow != null">
-        <div class="box w-[450px] mt-[150px] mb-0 mx-auto p-4 rounded-md shadow-lg text-base font-semibold text-center bg-zinc-50/90 dark:bg-zinc-900/90 text-gray-800 dark:text-zinc-300">
-          <!-- 제목 -->
-          <div>
-            <a target="_blank" :href="animeNow.website">
-              <!-- 한글제목 -->
-              <div class="text-xl font-bold">
-                {{animeNow.subject}}
-              </div>
-              <!-- 원워제목 -->
-              <div class="text-xs mt-1">
-                {{animeNow.originalSubject}}
-              </div>
-            </a>
-          </div>
-          <!-- 요일 -->
-          <div class="text-base mt-3" v-if="animeNow.period">{{animeNow.period}}</div>
-          <!-- 자막제작자 목록 -->
-          <div class="mt-3">
-            <div class="mt-2 text-base" v-for="node in captionList" :key="`${node.name}`">
-              <a v-if="node.website !== ''" :href="node.website" target="_blank">
-                <span>{{node.episodeText}}</span>
-                <b>&nbsp;{{node.name}}&nbsp;</b>
-                <span>{{node.updDtText}}</span>
+      <transition name="fade">
+        <div class="fixed inset-0 animate-fadeIn backdrop-blur-sm bg-gray-800/10" v-if="animeNow != null">
+          <div class="box min-[460px]:w-[450px] mt-[150px] mb-0 mx-auto p-4 rounded-md shadow-lg text-base font-semibold text-center bg-zinc-50/85 dark:bg-zinc-900/85 text-gray-800 dark:text-zinc-300">
+            <!-- 제목 -->
+            <div class="border-b border-gray-200 dark:border-zinc-800 pb-3">
+              <a target="_blank" :href="`/anime?animeNo=${animeNow.animeNo}`" class="hover:opacity-80">
+                <!-- 한글제목 -->
+                <div class="text-xl font-bold">
+                  {{animeNow.subject}}
+                </div>
+                <!-- 원워제목 -->
+                <div class="text-xs mt-1">
+                  {{animeNow.originalSubject}}
+                </div>
               </a>
-              <div v-else>
-                <span>{{node.episodeText}}</span>
-                <b>&nbsp;{{node.name}}&nbsp;</b>
-                <span>{{node.updDtText}}</span>
+            </div>
+            <!-- 요일 -->
+            <div class="mt-1 space-x-1 space-y-2 text-gray-800 dark:text-zinc-300 border-b pb-3 border-gray-200 dark:border-zinc-800 ">
+              <span class="as-tag-xs" v-for="tag in animeNow.tags" :key="tag">{{tag}}</span>
+            </div>
+            <!-- 자막제작자 목록 -->
+            <div class="mt-3">
+              <div class="mt-3 text-[15px]" v-for="node in captionList" :key="`${node.name}`">
+                <div v-if="node.website !== ''">
+                  <a :href="node.website" target="_blank" class="block hover:opacity-80">
+                    <span>{{node.episodeText}}</span> <span>{{node.name}}</span> <span>{{node.updDtText}}</span>
+                  </a>
+                </div>
+                <div v-else>
+                  <span>{{node.episodeText}}</span> <span>{{node.name}}</span> <span>{{node.updDtText}}</span>
+                </div>
               </div>
             </div>
+            <!-- 자막제작자 없음 -->
+            <div v-if="captionList.length === 0" class="text-[15px] font-normal leading-10 mt-0 mb-px mx-0 pt-10 pb-[50px] px-0">자막 제작자가 없습니다.</div>
           </div>
-          <!-- 자막없음 -->
-          <div v-if="captionList.length === 0" class="text-[15px] font-normal leading-10 mt-0 mb-px mx-0 pt-10 pb-[50px] px-0">자막이 없습니다.</div>
         </div>
-      </div>
+      </transition>
 
       <!-- 서버상태 체크 -->
       <div v-if="ajaxState.state == 'loading'" class="h-screen w-screen fixed flex items-center top-0 z-30 backdrop-blur-[2px] bg-white/20 dark:bg-black/10">
@@ -143,6 +148,7 @@ function evtClickClosePopup(event: MouseEvent) {
     animeNow.value = null;
   }
 }
+
 function evtKeyClosePopup(event: KeyboardEvent) {
   if (animeNow.value !== null && event.key === 'Escape') {
     animeNow.value = null;
@@ -162,3 +168,13 @@ onBeforeUnmount(() => {
   window.removeEventListener('keydown', evtKeyClosePopup, true);
 });
 </script>
+
+<!-- 애니메이션 CSS -->
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
